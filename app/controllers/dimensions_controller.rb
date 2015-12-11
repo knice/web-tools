3c888993ede9ee58123abfa21a1ae2171f6b75f8
Matt Congel <congel@chapman.edu>
2015-12-11 11:51:32 -0800

    Image model

--- app/controllers/dimensions_controller.rb
+++ app/controllers/dimensions_controller.rb
@@ -1,7 +1,7 @@
 class DimensionsController < ApplicationController
 
   def index
-
+    @image = Image.new
   end
 
 end
---
+++ app/models/image.rb
@@ -0,0 +1,9 @@
+class Image
+  include ActiveModel::Model
+
+  attr_accessor(
+    :image_upload,
+    :image_dimensions
+  )
+
+end
