cbfab9cfa3c8a6dc3c8823885bd0ef7e97124297
Matt Congel <congel@chapman.edu>
2016-01-14 17:03:59 -0800

    responsive croppie box sizing on image upload

--- app/assets/javascripts/dimensions.js
+++ app/assets/javascripts/dimensions.js
@@ -1,48 +1,63 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-var Resize = new function() {
+$(document).ready(function() {
+  var Resize = new function() {
 
-  var $uploadCrop;
-  this.viewWidth = document.getElementById('dimension_image_width');
-  this.viewHeight = document.getElementById('dimension_image_width');
+    var $uploadCrop;
 
-  this.newCroppie = function(e) {
-    var croppie = new Croppie(document.getElementById('upload-demo'), {
-      viewport: {
-        width: 200,
-        height: 200
-      },
-      boundary: {
-        width: 300,
-        height: 300
+    this.newCroppie = function(e, ratio) {
+      // FIX THE WIDTH WITH YOUR CSS MEDIA QUERIES AND TAKE OFF FLOAT LEFT ON THE COLUMNS FOR MOBILE
+      this.viewWidth = $('.upload-msg').width();
+      this.viewHeight = ratio * parseInt(this.viewWidth, 10);
+      console.log(this.viewWidth);
+      console.log(this.viewHeight);
+
+      var croppie = new Croppie(document.getElementById('upload-demo'), {
+        viewport: {
+          width: this.viewWidth,
+          height: this.viewHeight
+        },
+        boundary: {
+          width: parseInt(this.viewWidth, 10) + 100,
+          height: parseInt(this.viewHeight, 10) + 100
+        }
+      });
+      croppie.bind({
+        url: e.target.result
+      });
+
+      return croppie;
+    }
+
+    this.readFile = function(input) {
+      if(input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          var image = new Image();
+          image.src = e.target.result;
+
+          image.onload = function() {
+            console.log(this.width);
+            console.log(this.height);
+
+            var ratio = (this.height * 1.0) / this.width
+
+            $uploadCrop = Resize.newCroppie(e, ratio);
+            $('.upload-demo').addClass('ready');  
+          }
+        }
+
+        reader.readAsDataURL(input.files[0]);
+      } else {
+        alert("Sorry - you're browser doesn't support the FileReader API");
       }
-    });
-    croppie.bind({
-      url: e.target.result
-    });
-
-    return croppie;
-  }
-
-  this.readFile = function(input) {
-    if(input.files && input.files[0]) {
-      var reader = new FileReader();
-
-      reader.onload = function(e) {
-        $uploadCrop = Resize.newCroppie(e);
-        $('.upload-demo').addClass('ready');
-      }
-
-      reader.readAsDataURL(input.files[0]);
-    } else {
-      alert("Sorry - you're browser doesn't support the FileReader API");
     }
   }
-}
 
-$(document).on('change', '#dimension_image_upload', function () { Resize.readFile(this); });
-
+  $(document).on('change', '#dimension_image_upload', function () { Resize.readFile(this); });
+});
 // function popupResult(result) {
 //   var html;
 //   if (result.html) {
