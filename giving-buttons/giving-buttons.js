45e90233a33e257ec311a9c78dc27fa7aad28473
Lee White <leewhite128@gmail.com>
2016-04-19 09:40:53 -0700

    Update giving-buttons.js

--- giving-buttons/giving-buttons.js
+++ giving-buttons/giving-buttons.js
@@ -6,5 +6,5 @@ $("#url").bind("keyup blur paste", function() {
    var linkStr = "<a href='" + codeUrl + "'><img class='btn no-border' src='http://www.ucsc.edu/images/make-a-gift.png' alt='Make a Gift' /></a>";
   $("#code").text(linkStr);
   $(".preview").html(linkStr);
-  //$(".preview a").attr("target", "_new");
+  $(".preview a").attr("target", "_new");
 });
